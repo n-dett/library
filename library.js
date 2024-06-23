7d5e123d115e8b4d052682b3a951cc09c2c06dcd
n-dett <124851780+n-dett@users.noreply.github.com>
2024-06-23 11:39:37 -0500

    Set up initial layout

---
+++ library.js
@@ -0,0 +1,14 @@
+const library = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+
+
+function addBook(newBook) {
+    library.push(newBook);
+}
